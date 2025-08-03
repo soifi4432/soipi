8a9f6547903ae8b87acd41cb25769593b7af8a48
soifi4432 <rayyansyafiq653@gmail.com>
2025-08-03 14:55:48 +0700

    Add files via upload

---
+++ 20250803-145509.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "nama\t: Muhamad soifi" << endl;
+    cout << "alamat\t:Kp.cilumut girang desa mekarmulya" << endl;
+    cout << "tempat tanggal lahir\t:cianjur 25-11-2009" << endl;
+    cout << "skolah\t:Ma Al manshuriyah" << endl;
+    cout << "kelas\t:x ips" << endl;
+    cout << "hobi\t: main game" << endl;
+}
